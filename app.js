a10c834c351573892b68a6bc8e0ff3b4b7a346f9
oshanedesign <oshane.design@gmail.com>
2022-07-21 23:57:28 -0500

    a collection that keeps a list of books

---
+++ app.js
@@ -0,0 +1,5 @@
+list_of_books = [
+    { Title: 'Harry Potter', Author: 'JK Rowling'},
+    { Title: 'Hunger Games', Author: 'George newman'},
+    { Title: 'Freedom Avenue', Author: 'Miley Cyrus'}
+    ]
